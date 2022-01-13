a2da5bd6e95ed9263ca3b755819152b0a1b4b0f9
James Atkins <hello@jamesatkins.net>
2022-01-13 12:00:32 +0000

    Play Store price scraper: only check valid currency if app found
    For the price scraper, only check if the currency is valid or not if the app was found

--- cmd/play_store_scraper/scrape.go
+++ cmd/play_store_scraper/scrape.go
@@ -349,8 +349,9 @@ func ScrapeApp(ctx context.Context, client *http.Client, scrapedC chan<- Scraped
 				}
 
 				// Some apps don't have a valid currency but I think this is only free apps.
-				// Error if otherwise
-				if !details.Currency.Valid {
+				// Return Error if otherwise. Only check if the currency is valid or not if
+				// the app was found.
+				if !errors.Is(err, playstore.ErrAppNotFound) && !details.Currency.Valid {
 					return fmt.Errorf("paid app does not have currency: %s", appId)
 				}
 
