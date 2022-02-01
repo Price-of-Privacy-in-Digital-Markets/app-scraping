615f2f84f541533737922d9b6b37db300941be78
James Atkins <hello@jamesatkins.net>
2022-02-01 09:51:18 +0000

    Play Store: export to Parquet Python script
    Export to Parquet directly from the SQLite database instead of via a temporary JSONL file.
    Write prices too.

--- play_store_to_parquet.py
+++ play_store_to_parquet.py
@@ -1,11 +1,13 @@
 import argparse
-from pathlib import Path
+import datetime
+import io
+import sqlite3
 
 import pyarrow as pa
 import pyarrow.parquet as pq
 import pyarrow.json
 
-histogram_t = pa.struct([
+HISTOGRAM_T = pa.struct([
     ("1", pa.int64()),
     ("2", pa.int64()),
     ("3", pa.int64()),
@@ -13,7 +15,7 @@ histogram_t = pa.struct([
     ("5", pa.int64()),
 ])
 
-permission_t = pa.struct([
+PERMISSION_T = pa.struct([
     ("group", pa.string()),
     ("permission", pa.string()),
 ])
@@ -30,7 +32,7 @@ SCHEMA = pa.schema([
     pa.field("score", pa.float64()),
     pa.field("ratings", pa.int64()),
     pa.field("reviews", pa.int64()),
-    pa.field("histogram", histogram_t),
+    pa.field("histogram", HISTOGRAM_T),
     pa.field("price", pa.float64()),
     pa.field("currency", pa.string()),
     pa.field("sale", pa.bool_()),
@@ -64,19 +66,122 @@ SCHEMA = pa.schema([
     pa.field("comments", pa.list_(pa.string())),
     pa.field("editors_choice", pa.bool_()),
     pa.field("similar", pa.list_(pa.string())),
-    pa.field("permissions", pa.list_(permission_t))
+    pa.field("permissions", pa.list_(PERMISSION_T))
 ])
 
+PRICE_SCHEMA = pa.schema([
+    pa.field("scraped_when", pa.timestamp("s"), nullable=False),
+    pa.field("app_id", pa.string(), nullable=False),
+    pa.field("country", pa.string(), nullable=False),
+    pa.field("currency", pa.string(), nullable=False),
+    pa.field("price", pa.float64(), nullable=False),
+    pa.field("original_price", pa.float64(), nullable=True)
+])
+
+def scraped_apps(conn: sqlite3.Connection):
+    c = conn.cursor()
+    try:
+        # For parquet, similar is just a list of app IDs so we have to extract each ID from the
+        # similar object
+        c.execute(
+            """
+            SELECT
+                json_set(
+                    sa.json,
+                    '$.scraped_when', datetime(sa.scraped_when, 'unixepoch'),
+                    '$.similar', json_group_array(json_extract(similar.value, '$.app_id'))
+                )
+            FROM
+                (SELECT scrape_id, scraped_when, decompress_brotli(data) AS json FROM scraped_apps) AS sa,
+                json_each(sa.json, '$.similar') As similar
+            GROUP BY
+                scrape_id
+            """
+        )
+        for json_str, in c:
+            yield json_str.encode("utf-8")
+
+    finally:
+        c.close()
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Convert Play Store JSON to parquet.")
-    parser.add_argument("--input", type=str, required=True)
-    parser.add_argument("--output", type=str, required=True)
+    parser = argparse.ArgumentParser(description="Convert Play Store scraped data to parquet.")
+    parser.add_argument("--sqlite3-extension", type=str, default="sqlite3_tools")
+    parser.add_argument("--database", type=str, required=True)
+    parser.add_argument("--output-scraped", help="Path of scraped apps Parquet file", type=str, required=True)
+    parser.add_argument("--output-prices", help="Path of prices Parquet file", type=str, required=True)
 
     args = parser.parse_args()
 
-    tbl = pyarrow.json.read_json(
-        args.input,
-        parse_options=pa.json.ParseOptions(explicit_schema=SCHEMA, unexpected_field_behavior="ignore")
-    )
+    conn = sqlite3.connect(args.database)
+    try:
+        conn.enable_load_extension(True)
+
+        try:
+            conn.load_extension(args.sqlite3_extension)
+        except sqlite3.OperationalError:
+            print("Please set the path of the SQLite extension module.")
+            exit(1)
+
+        # Read the scraped apps from sqlite to a Python buffer (we need to have enough RAM) and
+        # then convert to an arrow table (this isn't that memory intensive as we can share the
+        # existing buffer), and then write to parquet.
+        # It would be nicer to be able to stream from sqlite without using so much memory but this
+        # works well enough for the time being.
+
+        print("Reading scraped apps...")
+        buf = io.BytesIO()
+
+        for scraped_app in scraped_apps(conn):
+            buf.write(scraped_app)
+            buf.write(b"\n")
+        
+        buf.seek(0)
+        
+        print("Creating arrow table...")
+        tbl = pyarrow.json.read_json(
+            pa.py_buffer(buf.getbuffer()),
+            parse_options=pyarrow.json.ParseOptions(explicit_schema=SCHEMA, unexpected_field_behavior="ignore")
+        )
+
+        print("Writing parquet file...")
+        pq.write_table(tbl, args.output_scraped, compression="ZSTD", compression_level=19)
+
+        del(buf)
     
-    pq.write_table(tbl, args.output, compression="ZSTD", compression_level=19)
+        # Now extract price information
+
+        print("Reading prices...")
+        rows = conn.execute(
+            """
+            SELECT
+                datetime(scraped_when, 'unixepoch'),
+                app_id,
+                country,
+                currency,
+                price,
+                original_price
+            FROM
+                prices
+            """
+        ).fetchall()
+
+        print("Creating arrow table...")
+        scraped_when, app_id, country, currency, price, original_price = zip(*rows)
+        tbl = pa.Table.from_pydict(
+            {
+                "scraped_when": [datetime.datetime.fromisoformat(dt) for dt in scraped_when],
+                "app_id": app_id,
+                "country": country,
+                "currency": currency,
+                "price": price,
+                "original_price": original_price
+            },
+            schema=PRICE_SCHEMA
+        )
+
+        print("Writing parquet file...")
+        pq.write_table(tbl, args.output_prices, compression="ZSTD", compression_level=19)
+
+    finally:
+        conn.close()
